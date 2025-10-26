552820fea3310a3a0d3a7a6ab29c689f3076ce89
User <user@example.com>
2025-10-25 21:22:28 -0500

    Update PyusdFaucet component

--- packages/nextjs/components/payvvm/PyusdFaucet.tsx
+++ packages/nextjs/components/payvvm/PyusdFaucet.tsx
@@ -29,6 +29,7 @@ export const PyusdFaucet = () => {
         setClaiming(false);
       });
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [faucet.signature]);
 
   const handleClaim = async () => {
